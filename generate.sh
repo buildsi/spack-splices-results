99aedef794df60394104519242830a2aec19f4b2
vsoch <vsoch@users.noreply.github.com>
2021-11-15 16:57:08 -0700

    try putting empty inputs for workflow dispatch
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- generate.sh
+++ generate.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 for dirname in $(ls artifacts/); do
-    printf "python visualize-predictions.py artifacts/$dirname\n"
-    python visualize-predictions.py artifacts/$dirname
+    outdir="docs/_results/${dirname}"
+    printf "Checking if we've generated results for ${dirname}\n"
+    if [[ ! -d "${outdir}" ]]; then
+        printf "python visualize-predictions.py artifacts/$dirname\n"
+        python visualize-predictions.py artifacts/$dirname
+    else
+        printf "$dirname results are already generated.\n"
+    fi
 done
