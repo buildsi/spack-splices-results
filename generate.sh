c7ad86d191f8377330d486a332a0ce8edb71d683
vsoch <vsoch@users.noreply.github.com>
2021-11-15 17:03:07 -0700

    try setting package variable
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- generate.sh
+++ generate.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-for dirname in $(ls artifacts/); do
-    outdir="docs/_results/${dirname}"
-    printf "Checking if we've generated results for ${dirname}\n"
-    if [[ ! -d "${outdir}" ]]; then
+if [ -z ${INPUT_PACKAGE+x} ]; then 
+    for dirname in $(ls artifacts/); do
         printf "python visualize-predictions.py artifacts/$dirname\n"
         python visualize-predictions.py artifacts/$dirname
-    else
-        printf "$dirname results are already generated.\n"
-    fi
-done
+    done   
+else 
+    printf "python visualize-predictions.py artifacts/${INPUT_PACKAGE}\n"
+    python visualize-predictions.py artifacts/${INPUT_PACKAGE}
+fi
